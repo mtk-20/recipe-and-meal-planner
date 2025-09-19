2d10786b52dbeaab78cf1195900ac19a782893cc
Myat Thukha <thukhamyat2000@gmail.com>
2025-09-19 09:08:05 +0630

    initial commit

---
+++ src/test/java/com/example/recipe_and_meal_planner/RecipeAndMealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe_and_meal_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAndMealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
